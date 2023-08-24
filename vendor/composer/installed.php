791ad42ae0456178676058aeda8c30520810e648
fidofear <firdah.yuliana@gmail.com>
2023-08-24 16:23:28 +0800

    add composer2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '832125640c31f6cb27539a9eba0c91952f35b0c3',
+        'reference' => '5208d4023c953fc1aa45ac60423628e8e3e39e2f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -436,7 +436,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '832125640c31f6cb27539a9eba0c91952f35b0c3',
+            'reference' => '5208d4023c953fc1aa45ac60423628e8e3e39e2f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -805,8 +805,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
